b98f464f0b793442d8750fbc065c61b48c6815c9
escano1 <estivenfrancocano@gmail.com>
2023-07-06 10:10:23 -0500

    adivinar numero

--- main.js
+++ main.js
@@ -3,8 +3,7 @@ const numeroAleatorio = Math.floor(Math.random() * 10) + 1;
 let intentos = 4;
 
 // Instrucciones para jugar
-alert('Tendras 4 oportunidades para ganar, si no sos un fracaso pa')
-alert('En la consola apareceran las pistas para dar con el numero correcto en caso de fallar')
+alert('Tendras 4 oportunidades para ganar, abre la consola para ver las pistas en caso de fallar')
 
 for (let i = 1; i <= intentos; i++) {
   // Solicitar al usuario que ingrese un número
@@ -26,6 +25,6 @@ for (let i = 1; i <= intentos; i++) {
         }
 
   } else {
-    alert("Lo siento, no eres capaz de adivinar un simple numero. El número era " + numeroAleatorio + ".");
+    alert("Lo siento, agotaste tus intentos. El número era " + numeroAleatorio + ".");
   }
 }
